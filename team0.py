0f63f3c3749a7402a9306cd41339035f7e8b7e07
fetzeret <fetzman4@gmail.com>
2017-11-06 10:01:45 -0600

    Ethan Fetzer

--- team0.py
+++ team0.py
@@ -6,9 +6,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'Ethan' # Only 10 chars displayed.
+strategy_name = 'Betray unless betrayed upon'
+strategy_description = 'Hope it works'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -26,7 +26,25 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
-    return 'c'
+    if len(my_history) <50:
+        return 'b'
+    if len(my_history) >=50:
+        if their_history[-1] == 'b':
+            return 'b'
+        if their_history[-1] == 'c':
+            return 'c'
+            
+    #import random 
+    #1 == 'b'
+    #2 == 'c'   
+    #choice=random.randint(1, 2) 
+    #return 
+    
+   
+        
+    
+    
+    
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
